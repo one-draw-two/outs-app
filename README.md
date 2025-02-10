b7ce3b3e28fb38ffff6971140f5c5e594d702659
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-02-10 14:26:09 +0100

    Capacitor iOs added and the PS conditional db logic fixed to separate between Android (IndexDB) vs Web + iOs (OPFS)

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 - With initial Powersync integtation: 467.01kb (the whole PS bundle is getting included, pre optimisation)
 - With PowerSync OPFS and two OPFS viewer functions (not used atm): 467.38kb
 - Feat: Capacitor added (only android), together with a build flag to detect web or cap: 467.68kb
+- Feat: Capacitor iOs added and the PS conditional db logic fixed to separate between Android (IndexDB) vs Web + iOs (OPFS): 470.88kb
--- plugins/usePowersync.ts
+++ plugins/usePowersync.ts
@@ -1,18 +1,20 @@
+import { Capacitor } from '@capacitor/core'
 import { PowerSyncDatabase, WASQLiteOpenFactory, WASQLiteVFS } from '@powersync/web'
 import { createPowerSyncPlugin } from '@powersync/vue'
 import { Connector } from '~/powersync/Connector'
 import { AppSchema } from '~/powersync/AppSchema'
 import { opfsNotSupportedMessage, purgeVFS, listVfsEntries } from '~/powersync/utils'
 
-// ToDo: Need to adjust the plag between iOs and Android properly
-// https://github.com/powersync-ja/powersync-js/blob/main/demos/example-capacitor/src/components/providers/SystemProvider.tsx
-
 export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig()
-  const isPlatformCapacitor = config.public.platform === 'cap' // Set in package.json (build:s > nuxt generate) script
+  // const isUseIndexDB = useRuntimeConfig().public.platform === 'cap' // Set in package.json (build:s > nuxt generate) script
+  // In package.json -> "build:s": "pnpm build && NUXT_PUBLIC_PLATFORM=cap pnpm generate && pnpm cap:sync", the NUXT_PUBLIC_PLATFORM=cap is unused at the moment
+  // An example from PS on how to use conditional db type: https://github.com/powersync-ja/powersync-js/blob/main/demos/example-capacitor/src/components/providers/SystemProvider.tsx
+
+  const isUseIndexDB = Capacitor.getPlatform() === 'android'
+
   const db = new PowerSyncDatabase({
     schema: AppSchema,
-    database: isPlatformCapacitor
+    database: isUseIndexDB
       ? {
           dbFilename: 'outs-ps-idb.db',
         }
@@ -20,7 +22,7 @@ export default defineNuxtPlugin((nuxtApp) => {
           dbFilename: 'outs-ps-opfs.db',
           vfs: WASQLiteVFS.OPFSCoopSyncVFS,
         }),
-    flags: isPlatformCapacitor
+    flags: isUseIndexDB
       ? {
           enableMultiTabs: typeof SharedWorker !== 'undefined',
         }
@@ -32,6 +34,6 @@ export default defineNuxtPlugin((nuxtApp) => {
 
   nuxtApp.vueApp.use(createPowerSyncPlugin({ database: db }))
 
-  nuxtApp.vueApp.config.globalProperties.$vfsPurge = isPlatformCapacitor ? opfsNotSupportedMessage : () => purgeVFS(db)
-  nuxtApp.vueApp.config.globalProperties.$vfsList = isPlatformCapacitor ? opfsNotSupportedMessage : listVfsEntries
+  nuxtApp.vueApp.config.globalProperties.$vfsPurge = isUseIndexDB ? opfsNotSupportedMessage : () => purgeVFS(db)
+  nuxtApp.vueApp.config.globalProperties.$vfsList = isUseIndexDB ? opfsNotSupportedMessage : listVfsEntries
 })
