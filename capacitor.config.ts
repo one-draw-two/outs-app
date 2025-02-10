1786ff7f5d0f7a13b01e061802da13be50719de2
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-02-10 15:40:15 +0100

    fix: bundle identifier changed to outs.tand.ing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'out.stand.ing',
+  appId: 'outs.tand.ing',
   appName: 'outstanding',
   webDir: 'dist',
   server: {
