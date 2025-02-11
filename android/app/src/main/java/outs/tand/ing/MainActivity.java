476cbc4724842f16d5e73bc1f3be7ac1fdfb1059
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-02-11 11:53:18 +0100

    fix: android package name

---
+++ android/app/src/main/java/outs/tand/ing/MainActivity.java
@@ -0,0 +1,5 @@
+package outs.tand.ing;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- layouts/season.vue
+++ layouts/season.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container mt-48">
     <SeasonPicker />
     <slot />
   </div>
