552d7ce7212dc74c8ce17fb4f6989d26c26122e6
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-02-12 12:43:17 +0100

    testing: refresh data in browser by purging

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <h1>Hello index</h1>
+  <button class="bg-blue-500" @click="listVFS">List VFS</button>
+  <button class="bg-red-500" @click="deleteVFS">Delete VFS</button>
 </template>
 
 <script setup lang="ts">
@@ -10,4 +12,21 @@ definePageMeta({
 useHead({
   title: 'Outstanding',
 })
+
+const { $vfsList, $vfsPurge }: any = useNuxtApp().vueApp.config.globalProperties
+
+const listVFS = () => {
+  console.log('Listando')
+  $vfsList()
+}
+
+const deleteVFS = async () => {
+  console.log('Deleting')
+  try {
+    await $vfsPurge()
+    console.log('Database cleanup completed')
+  } catch (error) {
+    console.error('Database cleanup failed:', error)
+  }
+}
 </script>
