4edea52547b8d27434b72483e4abaf17c0867843
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-04-10 14:15:20 +0200

    chore: renamed outs capacitor plugin

--- plugins/useCapacitor.client.ts
+++ plugins/useCapacitor.client.ts
@@ -3,7 +3,7 @@ import { App } from '@capacitor/app'
 import { Network } from '@capacitor/network'
 import { PushNotifications } from '@capacitor/push-notifications'
 import { FCM } from '@capacitor-community/fcm'
-import { CapLiveActivities } from 'outs-capacitor-liveactivities'
+import { CapLiveActivities } from '@outs1x2/capacitor-liveactivities'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   nuxtApp.provide('capacitor', {
