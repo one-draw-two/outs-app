dcaf2c2868c3710cd86e7816566352e15938302b
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-07-10 13:53:38 +0200

    test: inline logo in bg

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="h-12 main-container flex gap-8 relative">
-    <NuxtLink v-if="false" :to="useSL('')" class="block size-12 bg-gray-300"></NuxtLink>
-    <div class="flex items-center gap-4">
-      <NuxtLink :to="useSL('')" class="block"><img src="/outstanding.svg" class="h-4" alt="Logo" /></NuxtLink>
-      <AppSeasonPicker v-if="/(round|stage)/.test(useRoute().path) || true" />
-    </div>
-
+    <AppSeasonPicker>
+      <NuxtLink :to="useSL('')" class="block"><img src="/outstanding.svg" class="h-3" alt="Logo" /></NuxtLink>
+    </AppSeasonPicker>
     <AppUserButton />
   </div>
 </template>
