4e0da0672c559702fd8531fb8e89fa82d951fc61
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-07-10 13:27:18 +0200

    test: deploy with svg logo to see quality on phone

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="h-12 main-container flex gap-8 relative">
-    <NuxtLink :to="useSL('')" class="block size-12 bg-gray-300"></NuxtLink>
-    <AppSeasonPicker v-if="/(round|stage)/.test(useRoute().path) || true" />
+    <NuxtLink v-if="false" :to="useSL('')" class="block size-12 bg-gray-300"></NuxtLink>
+    <div class="flex items-center gap-4">
+      <NuxtLink :to="useSL('')" class="block"><img src="/outstanding.svg" class="h-4" alt="Logo" /></NuxtLink>
+      <AppSeasonPicker v-if="/(round|stage)/.test(useRoute().path) || true" />
+    </div>
+
     <AppUserButton />
   </div>
 </template>
