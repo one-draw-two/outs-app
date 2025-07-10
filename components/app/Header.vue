8a8eb1314fd7c63008e4c0a60fb08b72e54239e8
Kemal Yalcinkaya <kemalcany@gmail.com>
2025-07-10 14:24:44 +0200

    test label

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -5,9 +5,9 @@
       <NuxtLink :to="useSL('')" class="block"><img src="/outstanding.svg" class="h-3" alt="Logo" /></NuxtLink>
     </AppSeasonPicker>
     -->
-    <div class="flex flex-col h-12 w-48 relative">
-      <NuxtLink :to="useSL('')" class="block"><img src="/outstanding.svg" class="h-3" alt="Logo" /></NuxtLink>
-      <img src="/Season26.png" class="absolute -bottom-12 left-0 h-24" alt="Logo" />
+    <div class="flex flex-col xitems-center relative">
+      <NuxtLink :to="useSL('')" class="block"><img src="/outstanding.svg" class="absolute h-3 top-0.5" alt="Logo" /></NuxtLink>
+      <img src="/Season26Label.png" class="h-12" alt="Logo" />
     </div>
 
     <AppUserButton />
